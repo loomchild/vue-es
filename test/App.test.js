d85b267a7a8d94ffc64c42f85ca4d069e1a56392
Jarek Lipski <loomchild@loomchild.net>
2017-09-09 23:34:55 +0200

    Simplify test

--- test/App.test.js
+++ test/App.test.js
@@ -2,21 +2,22 @@ import Vue from 'vue'
 import App from '../src/App.vue'
 
 describe('App.vue', () => {
-  it('should have logo', () => {
+  let vm
+
+  beforeEach(() => {
     const Constructor = Vue.extend(App)
-    const vm = new Constructor().$mount()
+    vm = new Constructor().$mount()
+  })
+
+  it('should have logo', () => {
     expect(vm.$el.querySelector('#logo')).is.not.undefined
   })
 
   it('should have "Send event" button', () => {
-    const Constructor = Vue.extend(App)
-    const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('#send button').textContent).to.be.equal('Send event')
   })
 
   it('should have "Search events" button', () => {
-    const Constructor = Vue.extend(App)
-    const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('#search button').textContent).to.be.equal('Search events')
   })
 })
