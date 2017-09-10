80457099a3f2f9f0878b70f76dc5c5e42c110187
Jarek Lipski <loomchild@loomchild.net>
2017-09-10 23:10:32 +0200

    Use ES6 exports

--- src/logic/events.js
+++ src/logic/events.js
@@ -1,11 +1,9 @@
-module.exports = {
-
-  create () {
-    console.log('AAA')
-  },
-
-  search (criteria) {
-    console.log('BBB ' + criteria)
-  }
-
+const create = () => {
+  console.log('AAA')
 }
+
+const search = (criteria) => {
+  console.log('BBB ' + criteria)
+}
+
+export {create, search}
